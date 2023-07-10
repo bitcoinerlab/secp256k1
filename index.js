a97b76c801394115aceaabed3d3b0aa941f74059
José Luis Landabaso Díaz <landabaso@gmail.com>
2023-07-10 11:04:17 +0200

    Revert signSchnorr to use default all-zero Buffer for auxRand
    This commit reverts the behavior of the `signSchnorr` function to use an all-zero Buffer when `auxRand` isn't provided, in alignment with the specifications outlined by `bitcoinjs-lib` for `secp256k1`. This update ensures consistency between the two libraries and prevents discrepancies in Schnorr signatures. Additionally, version number is incremented to 1.0.5 to reflect this change.

--- index.js
+++ index.js
@@ -310,17 +310,15 @@ export function sign(h, d, e) {
   return necc.signSync(h, d, { der: false, extraEntropy: e });
 }
 
-export function signSchnorr(h, d, e) {
-  if (e === undefined) {
-    console.warn(
-      `Warning: The extra data 'e' is not defined. This library defaults to a random value when 'e' is undefined, which is different from the deterministic approach in tiny-secp256k1. This might lead to discrepancies in the Schnorr signatures between the two libraries.`
-    );
-  }
+export function signSchnorr(h, d, e = Buffer.alloc(32, 0x00)) {
   if (!isPrivate(d)) {
     throw new Error('Expected Private');
   }
   if (!isHash(h)) {
     throw new Error('Expected Scalar');
+  }
+  if (!isExtraData(e)) {
+    throw new Error('Expected Extra Data (32 bytes)');
   }
   return necc.schnorr.signSync(h, d, e);
 }
