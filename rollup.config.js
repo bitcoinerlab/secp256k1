8ec598da44aab302a95c78e000cac4e250bc6071
landabaso <landabaso@gmail.com>
2026-08-17 19:23:16 +0200

    Refresh development dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,7 @@
-import commonjs from 'rollup-plugin-commonjs';
-
 export default {
   input: './index.js',
   output: {
     file: './dist/index.js',
     format: 'cjs'
-  },
-  plugins: [commonjs()]
+  }
 };
